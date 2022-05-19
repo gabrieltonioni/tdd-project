1b652314f7fb53a4a165c9af0d3c43caa923bace
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-05-19 16:23:38 -0300

    Regressão

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
